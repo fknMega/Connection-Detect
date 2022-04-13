aa7c3fa50ad703fdd771af25b42ff230899cc655
fknMega <yanivwhat@gmail.com>
2022-04-13 15:08:23 +0300

    Add project files.

---
+++ Connection Detect/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using netstatPlus;
+using Newtonsoft.Json;
+
+
+
+
+
+namespace Connection_Detect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Parallel.ForEach(netclient.GetConnections(), connection =>
+            {
+
+                WebClient client = new WebClient();
+
+                var content = client.DownloadString("http://ipwhois.app/json/" + connection.remoteaddy);
+                //Console.WriteLine(content);
+                dynamic json = JsonConvert.DeserializeObject(content);
+                if (json.success == true)
+                {
+
+                    Console.WriteLine(connection.name + " (" + connection.pid + ")" + " >> >> " + connection.remoteaddy + " location: " + json.country + ", " + json.city + "\n");
+
+                }
+
+
+            });
+
+
+        }
+    }
+}
